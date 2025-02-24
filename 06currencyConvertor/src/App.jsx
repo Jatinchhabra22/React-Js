69801a567a1032b74ef0a56500977f75b1dacb47
Jatinchhabra22 <147986920+Jatinchhabra22@users.noreply.github.com>
2025-02-24 17:57:07 +0530

    06

---
+++ 06currencyConvertor/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+    
+  return (
+    <>
+      <h1 className='text-8xl text-red-700 bg-red-300'>  currency app</h1>
+    </>
+  )
+}
+
+export default App
